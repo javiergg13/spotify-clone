d0eeb5f04bb5c5d77a6082d655db87ec8401d06e
jguerrero <jguerrero@cgi.es>
2023-10-11 13:20:38 +0200

    v1

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,38 @@
-export function CardPlayButton({ id }) {
+import { Pause, Play } from "./Player"
+import { usePlayerStore } from '@/store/playerStore'
+
+export function CardPlayButton ({ id, size = 'small' }) {
+  const {
+    currentMusic,
+    isPlaying,
+    setIsPlaying,
+    setCurrentMusic
+  } = usePlayerStore(state => state)
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
+
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false)
+      return
+    }
+
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data
+
+        setIsPlaying(true)
+        setCurrentMusic({ songs, playlist, song: songs[0] })
+      })
+  }
+
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5'
+
+
   return (
-    <div className="card-play-button">
-      <button>Play</button>
-    </div>
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
+    </button>
   )
 }
--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
 
-  let greeting = "";
+  let greeting = ""
 
   if (currentHour < 12) {
-    greeting = "Buenos días";
+    greeting = "Buenos días"
   } else if (currentHour < 18) {
-    greeting = "Buenas tardes";
+    greeting = "Buenas tardes"
   } else {
-    greeting = "Buenas noches";
+    greeting = "Buenas noches"
   }
 </script>
 
-<h1 class="text-4xl font-bold">{greeting}</h1>
+<h1 class="text-3xl font-bold">{greeting}</h1>
