ff09b4f0aa8a34eb49b0065306188aaa394366a9
javiergg13 <javiergg13@educastur.es>
2024-06-10 23:21:13 +0200

    actulizar astro, netlify y lanzar run build

---
+++ .netlify/functions-internal/ssr/chunks/get-info-playlist-json_HqYgl7Ce.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/get-info-playlist-json_CW37XPbE.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/pages/get-info-playlist-json_CW37XPbE.mjs
@@ -0,0 +1,17 @@
+import { a as allPlaylists, s as songs } from './_id__DgmWWqiH.mjs';
+
+async function GET({ params, request }) {
+  // get the id from the url search params
+  const { url } = request;
+  const urlObject = new URL(url);
+  const id = urlObject.searchParams.get('id');
+
+  const playlist = allPlaylists.find((playlist) => playlist.id === id);
+  const songs$1 = songs.filter(song => song.albumId === playlist?.albumId);
+
+  return new Response(JSON.stringify({ playlist, songs: songs$1 }), {
+    headers: { "content-type": "application/json" },
+  })
+}
+
+export { GET };
